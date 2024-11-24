0b832d2c5003cc384da783a1c53820bd988f4e41
Amy Galbicsek <acgalbicsek@gmail.com>
2024-11-23 15:52:26 -0900

    updated work

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FaveSubject();
+
+        }
+
+        public static void FaveSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();    
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I do not like Math");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is awesome!");
+                    break;
+
+                case "p.e.":
+                case "pe":
+                case "gym":
+                    Console.WriteLine("Gym is alright");
+                    break;
+                case "history":
+                    Console.WriteLine("History is my favorite!");
+                    break;
+
+                case "geography":
+                    Console.WriteLine("Geography is boring");
+                    break;
+
+                default: Console.WriteLine("Nahhhh");
+                    break;
+
+
+
+            }
         }
     }
 }
